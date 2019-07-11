4b6cd55c529f7e3667829f8442b47be38a066f02
Tom Hatzer <tom.hatzer@antiloop.com>
2019-07-11 18:09:06 +0200

    First version that removes the start unhandled promise exception

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,8 +2,10 @@ let mix = require('laravel-mix')
 let tailwindcss = require('tailwindcss')
 
 mix.setPublicPath('dist')
-   .js('resources/js/nova.js', 'js')
-   .postCss('resources/sass/app.css', 'css', [
-       tailwindcss('./tailwind.js'),
+   .postCss('../../vendor/laravel/nova/resources/css/app.css', './css/app.css', [
+       tailwindcss('../../vendor/laravel/nova/tailwind.js'),
        require('postcss-css-variables')()
-   ]);   
+   ])
+  .babel('../../vendor/laravel/nova/public/manifest.js', './dist/js/manifest.babel.js')
+  .babel('../../vendor/laravel/nova/public/vendor.js', './dist/js/vendor.babel.js')
+  .babel('../../vendor/laravel/nova/public/app.js', './dist/js/nova.babel.js');
